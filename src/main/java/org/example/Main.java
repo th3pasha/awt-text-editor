8bd80946fefbf755c31779e6f0239ed8a95a0e77
Ayoub <elpasha117@gmail.com>
2023-01-06 16:30:38 +0100

    fin

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,44 +1,9 @@
 package org.example;
 
-import java.awt.*;
-import java.io.File;
-import java.io.FileWriter;
-
 public class Main {
     public static void main(String[] args) {
 
         TextEditor textEditor =new TextEditor();
-
-
-        //Frame f = new Frame();
-
-        /*
-
-        FileDialog fd = new FileDialog(f, "", FileDialog.SAVE);
-        fd.setVisible(true);
-        System.out.println(fd.getDirectory()) + "\";
-
-        String fileName = "salam.txt";
-        File f = new File(get+ "\\" + fileName);
-
-        /*
-
-        Frame f = new Frame();
-        FileDialog fd = new FileDialog(f, "", FileDialog.SAVE);
-        fd.setVisible(true);
-        System.out.println(fd.getFile());
-        String str = "sALm";
-        File savedFile = new File(fd.getFile());
-        try
-        {
-            FileWriter fileWriter = new FileWriter(savedFile);
-            fileWriter.write(str);
-            fileWriter.close();
-        }
-        catch(Exception exception){
-
-        }
-        */
 
     }
 }
--- src/main/java/org/example/TextEditor.java
+++ src/main/java/org/example/TextEditor.java
@@ -7,29 +7,112 @@ import java.util.Scanner;
 
 public class TextEditor extends Frame implements ActionListener
 {
-    private String text;
+    String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+    //Choice fontPane;
+    MenuBar menuBar;
+    TextArea textArea;
 
-    // les MenuBar
-    MenuBar menuBar = new MenuBar();
-    // Zone de text
-    TextArea textArea = new TextArea();
+    Dialog saveDialog;
 
-    Frame frame = new Frame();
-    // les Menu File, Edit, Save
-    Menu fileMenu = new Menu("File", true);
-    Menu editMenu = new Menu("Edit");
+    Menu fileMenu;
+    Menu editMenu;
+    Menu formatMenu;
 
-    // les MenuItem Open, Close
-    MenuItem fileOpen = new MenuItem("Open");
-    MenuItem fileClose = new MenuItem("Close");
-    MenuItem fileSaveAs = new MenuItem("Save as");
+    MenuItem fileNew;
+    MenuItem fileOpen;
+    MenuItem fileClose;
+    MenuItem fileSaveAs;
+    MenuItem fileExit;
 
+    MenuItem editSelect;
+    MenuItem policeFormat;
+    MenuItem fontFormat;
 
+    public TextEditor()
+    {
+        menuBar = new MenuBar();
+        editMenu    = new Menu("Edit");
+        textArea = new TextArea();
+       //fontPane = new Choice();
+        editSelect  = new MenuItem("Select all");
+
+        saveDialog = new Dialog(this);
+
+        setMenuBar(menuBar);
+        add(textArea);
+        //add(fontPane);
+
+        setVisible(true);
+        textArea.setFont(new Font("Arial",Font.PLAIN,20));
+        setTitle("untitled.txt - Editeur de texte");
+        setSize(600, 600);
+
+        fileMenu = new Menu("File", true);
+        fileNew = new MenuItem("New");
+        fileOpen = new MenuItem("Open");
+        fileClose = new MenuItem("Close");
+        fileSaveAs = new MenuItem("Save as");
+        fileExit = new MenuItem("Exit");
+
+        formatMenu   = new Menu("Format");
+        policeFormat = new MenuItem("Police");
+        fontFormat   = new MenuItem("Font");
+
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        menuBar.add(formatMenu);
+        editMenu.add(editSelect);
+
+        fileMenu.add(fileNew);
+        fileMenu.add(fileOpen);
+        fileMenu.add(fileClose);
+        fileMenu.add(fileSaveAs);
+        fileMenu.addSeparator();
+        fileMenu.add(fileExit);
+
+        formatMenu.add(fontFormat);
+        formatMenu.add(policeFormat);
+
+         /*
+        for (int i = 0; i <= fonts.length - 50 ;i++){
+            fontPane.add(fonts[i]);
+        }
+         */
+        //fontPane.setBounds(50);
+
+        fileNew.addActionListener(this);
+        fileOpen.addActionListener(this);
+        fileClose.addActionListener(this);
+        fileSaveAs.addActionListener(this);
+        fileExit.addActionListener(this);
+        editSelect.addActionListener(this);
+
+        /*
+        fontPane.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                textArea.setFont(new Font((String)fontPane.getSelectedItem(),Font.PLAIN,textArea.getFont().getSize()));
+            }
+        });*/
+
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
     @Override
-    public void actionPerformed(ActionEvent e) {
-        String s = e.getActionCommand();
+    public void actionPerformed(ActionEvent e)
+    {
+        Frame frame = new Frame();
 
-        if (s.equals("Open"))
+        if (e.getActionCommand().equals("New"))
+        {
+            setTitle("untitled.txt - Editeur de texte");
+            textArea.setText("");
+        }
+        if (e.getActionCommand().equals("Open"))
         {
             String data = null;
             FileDialog fileLoadDialog = new FileDialog(frame, "loadDialog", FileDialog.LOAD);
@@ -49,9 +132,9 @@ public class TextEditor extends Frame implements ActionListener
             {
                 throw new RuntimeException(ex);
             }
+            setTitle(fileLoadDialog.getFile() + " - Editeur de texte ");
         }
-
-        if (s.equals("Save as"))
+        if (e.getActionCommand().equals("Save as"))
         {
             FileDialog fileLoadDialog = new FileDialog(frame, "saveDialog", FileDialog.SAVE);
             fileLoadDialog.setVisible(true);
@@ -63,46 +146,26 @@ public class TextEditor extends Frame implements ActionListener
                 bufferedWriter.write(textArea.getText());
                 bufferedWriter.close();
             }
-            catch(Exception exception)
+            catch(Exception ex)
             {
-
+                throw new RuntimeException(ex);
             }
+            setTitle(fileLoadDialog.getFile()+ " - Editeur de texte ");
         }
-    }
 
-    public TextEditor()
-    {
-
-        setTitle("Text Editor");
-        setBounds(100, 100, 600, 700);
-        setVisible(true);
-        setMenuBar(menuBar);
-        add(textArea);
-
-        // ----------------------------------------------- Editeur de text setting
-        this.text = textArea.getSelectedText();
-
-        // ----------------------------------------------- les zone dans MenuBar
-        menuBar.add(fileMenu);
-        menuBar.add(editMenu);
-
-        // -------------------------------------------------le Menu file
-        fileMenu.add(fileOpen);
-        fileMenu.add(fileClose);
-        fileMenu.add(fileSaveAs);
-
-        // ----------------------------------------------
-        fileOpen.addActionListener(this);
-        fileClose.addActionListener(this);
-        fileSaveAs.addActionListener(this);
-
-        // --------------------------- WINDOWS LISTENERS ----------------------------
-
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
+        if (e.getActionCommand().equals("Close"))
+        {
+            saveDialog.setVisible(true);
+            saveDialog.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    saveDialog.dispose();
+                }
+            });
+        }
+        if (e.getActionCommand().equals("Exit"))
+        {
+            dispose();
+        }
     }
 }
